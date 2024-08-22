ae63eb05b1666b82b347205b257f731b87fb1a34
gabriel giacometti <gabrielhdfg460@gmail.com>
2024-08-22 19:07:39 -0300

    reafactoring code

--- src/main/java/com/restaurante/web/reserva/ReservaController.java
+++ src/main/java/com/restaurante/web/reserva/ReservaController.java
@@ -1,6 +1,8 @@
 package com.restaurante.web.reserva;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,71 +11,47 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.restaurante.Command;
 import com.restaurante.web.ControllerHelper;
-import com.restaurante.web.HomeServlet;
+
 
 @WebServlet("/reserva/*")
 public class ReservaController extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
+	private final Map<String, Command> commandMap = new HashMap<>();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void init() throws ServletException {
+
+		commandMap.put("/reserva/agendar", new CreateReserva());
+		commandMap.put("/reserva/reservaInfo", new ReservaInfo());
+		commandMap.put("/reserva/confirmada", new ReservaConfirmed());
+		commandMap.put("/reserva/negada", new ReservaNegada());
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request(request, response);
 	}
 
-private void request(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		
+	private void request(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
 		try {
-			
-			Command cmd = getCommand(request);
+
+			Command cmd = commandMap.get(ControllerHelper.extractOperation(request));
 			cmd.execute(request, response);
-			
+
 		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
 			throw new ServletException(e);
 		}
 
 	}
-	
-	private Command getCommand(HttpServletRequest request) {
-		String operation = ControllerHelper.extractOperation(request);
-		System.out.println(operation);
-		if (operation.matches(".*\\.(css|js|png|jpg|jpeg|gif|svg)$")) {
-	        return null;
-	    }
-		
-		Command cmd = null;
-		switch (operation) {
-			case "/reserva/agendar":
-				cmd = new CreateReserva();
-				break;
-			
-			case "/reserva/reservaInfo":
-				cmd = new ReservaInfo();
-				break;
-				
-			case "/reserva/confirmada":
-				cmd = new ReservaConfirmed();
-				break;
-			case "/reserva/negada":
-				cmd = new ReservaNegada();
-				break;
-			
-			default:
-	            System.err.println("Operação desconhecida: " + operation);
-	            break;
-		
-		}
-		
-		return cmd;
-	}
 
-
-}	
-
+}
